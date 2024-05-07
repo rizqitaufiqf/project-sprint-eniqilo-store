436033909cf92321bff6477b40c0457a3d9a67ac
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-05-07 12:00:20 +0700

    remove unused custom validator

--- helpers/custom_validator.go
+++ helpers/custom_validator.go
@@ -2,17 +2,9 @@ package helpers
 
 import (
 	"regexp"
-	"strconv"
 
 	"github.com/go-playground/validator"
 )
-
-func validateBoolean(fl validator.FieldLevel) bool {
-	value := fl.Field().String()
-	_, err := strconv.ParseBool(value)
-
-	return err == nil
-}
 
 func validatePhoneNumber(fl validator.FieldLevel) bool {
 	value := fl.Field().String()
@@ -26,6 +18,5 @@ func RegisterCustomValidator(validator *validator.Validate) {
 	// validator.RegisterValidation() -> if you want to create new tags rule to be used on struct entity
 	// validator.RegisterStructValidation() -> if you want to create validator then access all fields to the struct entity
 
-	validator.RegisterValidation("boolean", validateBoolean)
 	validator.RegisterValidation("phoneNumber", validatePhoneNumber)
 }
