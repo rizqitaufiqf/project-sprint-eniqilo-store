70ffb9b298cf4fc7d9c05c722c6401ccc037c4fc
raihan_parl <raihan.parl4@gmail.com>
2024-05-10 01:17:06 +0700

    update product details to jsonb

--- database/migrations/20240507111229_add_transaction_table.up.sql
+++ database/migrations/20240507111229_add_transaction_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS transactions (
     id VARCHAR(100) NOT NULL PRIMARY KEY,
     customer_id VARCHAR(100) NOT NULL,
-    product_details VARCHAR[] NOT NULL,
+    product_details jsonb NOT NULL,
     paid INT NOT NULL,
     change INT NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE NO ACTION,
