7cc57b676506a325ec375769abf9fccafa6a8da0
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-05-10 09:16:41 +0700

    add repository

--- repository/product/product_repository_impl.go
+++ repository/product/product_repository_impl.go
@@ -3,8 +3,13 @@ package product_repository
 import (
 	"context"
 	product_entity "eniqilo-store/entity/product"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -36,7 +41,75 @@ func (repository *productRepositoryImpl) Add(ctx context.Context, product produc
 	}
 
 	product.Id = productId
-	product.CreatedAt = createdAt.Format(time.RFC3339)
+	product.CreatedAt = createdAt
 
 	return &product, nil
+}
+
+func (repository *productRepositoryImpl) CustomerSearch(ctx context.Context, searchQuery product_entity.ProductCustomerSearch) (*[]product_entity.Product, error) {
+	query := "SELECT id, name, sku, category, image_url, notes, stock, price, location, is_available, is_deleted, created_at FROM products WHERE is_deleted = FALSE AND is_available = TRUE"
+	var whereClause []string
+	params := []interface{}{}
+
+	if searchQuery.Name != "" {
+		whereClause = append(whereClause, fmt.Sprintf("name ~* $%s", strconv.Itoa(len(params)+1)))
+		params = append(params, searchQuery.Name)
+	}
+
+	log.Println(searchQuery.Category)
+
+	if searchQuery.Category != "" {
+		whereClause = append(whereClause, fmt.Sprintf("category = $%s", strconv.Itoa(len(params)+1)))
+		params = append(params, searchQuery.Category)
+	}
+
+	if searchQuery.Sku != "" {
+		whereClause = append(whereClause, fmt.Sprintf("sku = $%s", strconv.Itoa(len(params)+1)))
+		params = append(params, searchQuery.Sku)
+	}
+
+	if searchQuery.InStock != "" {
+		inStock, err := strconv.ParseBool(searchQuery.InStock)
+		if err != nil {
+			return &[]product_entity.Product{}, err
+		}
+		var operator string
+		if inStock {
+			operator = ">"
+		} else {
+			operator = "="
+		}
+		whereClause = append(whereClause, fmt.Sprintf("stock %s 0", operator))
+	}
+
+	if len(whereClause) > 0 {
+		query += " AND " + strings.Join(whereClause, " AND ")
+	}
+
+	if searchQuery.Price != "" {
+		query += fmt.Sprintf(" ORDER BY price %s", searchQuery.Price)
+	} else {
+		query += " ORDER BY created_at DESC"
+	}
+
+	if searchQuery.Limit > 0 {
+		query += fmt.Sprintf(" LIMIT $%s OFFSET $%s", strconv.Itoa(len(params)+1), strconv.Itoa(len(params)+2))
+		params = append(params, searchQuery.Limit)
+		params = append(params, searchQuery.Offset)
+	}
+	log.Println(query)
+	log.Println(params...)
+
+	rows, err := repository.dbPool.Query(ctx, query, params...)
+	if err != nil {
+		return &[]product_entity.Product{}, err
+	}
+	defer rows.Close()
+
+	products, err := pgx.CollectRows(rows, pgx.RowToStructByName[product_entity.Product])
+	if err != nil {
+		return &[]product_entity.Product{}, err
+	}
+
+	return &products, nil
 }
